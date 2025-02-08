a1e928022b7770f8b96242759930c058f72cf170
Wh1isper <43375501+Wh1isper@users.noreply.github.com>
2025-02-08 04:20:37 +0000

    Bump to next micro version

--- langfuse_pydantic_ai/__init__.py
+++ langfuse_pydantic_ai/__init__.py
@@ -2,7 +2,7 @@
 
 __author__ = "wh1isper"
 __email__ = "jizhongsheng957@gmail.com"
-__version__ = "0.0.3.dev0"
+__version__ = "0.0.4.dev0"
 
 
 from ._warpper import observe_model, observed_agent, use_observed_agent

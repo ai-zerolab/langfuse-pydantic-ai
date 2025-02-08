4001fa6472f4ec0ca985fa2b252a14d9e5704db3
Wh1isper <jizhongsheng957@gmail.com>
2025-02-08 11:53:16 +0800

    Imp observe for pydantic-ai

--- langfuse_pydantic_ai/_warpper.py
+++ langfuse_pydantic_ai/_warpper.py
@@ -1,24 +1,45 @@
+from contextlib import asynccontextmanager
 from functools import wraps
-from typing import Any, AsyncIterator
+from inspect import signature
+from typing import Any, AsyncIterator, Coroutine
 
 from langfuse.decorators import langfuse_context, observe
 from pydantic_ai.agent import Agent
 from pydantic_ai.models import Model, StreamedResponse
-from pydantic_ai.result import RunResult
+from pydantic_ai.result import Usage
+from pydantic_ai.messages import ModelResponse, ModelMessage
 
 
 def _warp_model_request(model: Model) -> Model:
     origin_request = model.request
-
-    @observe(name="model-request", as_type="generation")
+    sig = signature(origin_request)
     @wraps(origin_request)
-    async def _warpped(*args: Any, **kwargs: Any) -> RunResult[str]:
+    @observe(name="model-request", as_type="generation")
+    async def _warpped(
+        *args: Any, **kwargs: Any
+    ) -> Coroutine[Any, Any, tuple[ModelResponse, Usage]]:
+        bound_args = sig.bind(*args, **kwargs)
+        bound_kwargs = dict(bound_args.arguments)
 
-        model_settings = model.settings
+        langfuse_context.update_current_observation(
+            input=bound_kwargs["messages"],
+            model=model.model_name,
+            model_parameters=bound_kwargs.get("model_parameters", None),
+            metadata=bound_kwargs,
+        )
 
-        result = origin_request(*args, **kwargs)
+        response, usage = await origin_request(*args, **kwargs)
 
-        return result
+        langfuse_context.update_current_observation(
+            output=response,
+            usage={
+                "input": usage.request_tokens,
+                "output": usage.response_tokens,
+                "total": usage.total_tokens,
+            },
+        )
+
+        return response, usage
 
     model.request = _warpped
 
@@ -27,11 +48,34 @@ def _warp_model_request(model: Model) -> Model:
 
 def _warp_model_request_stream(model: Model) -> Model:
     origin_request_stream = model.request_stream
+    sig = signature(origin_request_stream)
 
-    @observe(name="model-request-stream", as_type="generation")
     @wraps(origin_request_stream)
+    @asynccontextmanager
+    @observe(name="model-request-stream", as_type="generation")
     async def _warpped(*args: Any, **kwargs: Any) -> AsyncIterator[StreamedResponse]:
-        return origin_request_stream(*args, **kwargs)
+        bound_args = sig.bind(*args, **kwargs)
+        bound_kwargs = dict(bound_args.arguments)
+
+        langfuse_context.update_current_observation(
+            input=bound_kwargs["messages"],
+            model=model.model_name,
+            model_parameters=bound_kwargs.get("model_parameters", None),
+            metadata=bound_kwargs,
+        )
+
+        response: StreamedResponse
+        async with origin_request_stream(*args, **kwargs) as response:
+            yield response
+            usage = response.usage()
+            langfuse_context.update_current_observation(
+                output=response.get(),
+                usage={
+                    "input": usage.request_tokens,
+                    "output": usage.response_tokens,
+                    "total": usage.total_tokens,
+                },
+            )
 
     model.request_stream = _warpped
 
